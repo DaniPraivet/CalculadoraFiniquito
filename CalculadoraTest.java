dbb8a92c05f083884252d257dacc966d36b8f2ee
rodriguez.pedan <rodriguez.pedan24@cordoba.salesianos.edu>
2025-04-03 20:35:02 +0200

    Creamos el test de getDiasHastaFecha

---
+++ CalculadoraTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+    @Test
+    void testGetDiasHastaFecha() {
+        assertEquals(31,Calculadora.getDiasHastaFecha(31,1));
+        assertEquals(59,Calculadora.getDiasHastaFecha(59,2));
+        assertEquals(90,Calculadora.getDiasHastaFecha(90,3));
+    }
+}
